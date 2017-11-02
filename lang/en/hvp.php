98473e43d9a7ed4c1f68e117416d58edee63183d
Lillian Hawasli <lfhawasli@gmail.com>
2017-11-02 16:36:16 -0700

    CCLE-7048 - Added "View all submissions" to h5p left navigation

--- lang/en/hvp.php
+++ lang/en/hvp.php
@@ -351,6 +351,9 @@ $string['licenseCC010'] = 'CC0 1.0 Universal (CC0 1.0) Public Domain Dedication'
 $string['licenseCC010U'] = 'CC0 1.0 Universal';
 $string['licenseversion'] = 'License Version';
 
+// START UCLA MOD: CCLE-7048 - Add "View all submissions" link to H5P Quiz administration block.
+$string['viewgrading'] = 'View all submissions';
+// END UCLA MOD: CCLE-7048.
 // Embed.
 $string['embedloginfailed'] = 'You do not have access to this content. Try logging in.';
 
